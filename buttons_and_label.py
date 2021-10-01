412820c67894d8798b965469a07f41e9c6c683cd
iian armas <armas.cav@gmail.com>
2021-10-02 02:49:28 +0800

    Update buttons_and_label.py

--- buttons_and_label.py
+++ buttons_and_label.py
@@ -91,3 +91,78 @@ window.show()
 
 # exit
 sys.exit(app.exec())
+
+
+
+# ==================================================================
+# CLEANER VERSION WITH LABEL ADJUSTMENT METHOD
+
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QLabel, QGraphicsDropShadowEffect
+from PyQt6.QtGui import QIcon, QFont, QColor
+
+import sys
+
+
+class Window(QWidget):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setGeometry(400, 200, 780, 460)
+        self.setWindowTitle('Iian')
+        self.setStyleSheet('background-color: #f4f3f5')
+
+        self.label = QLabel(self)
+        self.btn_1 = QPushButton(self)
+
+        self.shadow = QGraphicsDropShadowEffect()
+        self.shadow.setBlurRadius(24)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(8)
+        self.shadow.setColor(QColor('#e4e3e5'))
+
+        self.btn_1_counter = 1
+        self.initUI()
+
+    def initUI(self):
+        self.label.setText('Hello World')
+        self.label.move(int(290 + 171/5.2), 80)
+        self.label.setStyleSheet('color: #191a18')
+        self.label.setFont(QFont('Arial', 20))
+
+        btn_stylesheet = (
+            'background-color: #fcfcfc;'
+            'border-radius: 15'
+        )
+
+        self.btn_1.setText('Click Here')
+        self.btn_1.setGeometry(290, 300, 171, 61)
+        self.btn_1.setStyleSheet(btn_stylesheet)
+        self.btn_1.setIcon(QIcon('newlogo.png'))
+        self.btn_1.setGraphicsEffect(self.shadow)
+        self.btn_1.released.connect(self.on_btn_1_release)
+
+    def on_btn_1_release(self):
+        self.btn_1.setText('Click Here Again')
+        num = 'time' if self.btn_1_counter == 1 else 'times'
+        self.label.setText(f'Clicked {self.btn_1_counter} {num}')
+        self.update()
+        self.btn_1_counter += 1
+
+    def update(self):
+        # this will adjust the size of the label container to fit the new character numbers
+        self.label.adjustSize()
+
+
+def start_app():
+
+    app = QApplication(sys.argv)
+
+    win = Window()
+
+    win.show()
+
+    sys.exit(app.exec())
+
+
+start_app()
+
