d073c267f8e8e7bc73bb220625b7f170b9b20ae0
iian armas <armas.cav@gmail.com>
2021-10-03 09:16:16 +0800

    Update new_window_launch.py

--- new_window_launch.py
+++ new_window_launch.py
@@ -1,13 +1,20 @@
 # =========== MAIN.PY =========== #
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox, QPushButton
 from dialog import Ui_Dialog
 from dash import Ui_MainWindow
+from sheet import style
+
+from qt_material import apply_stylesheet
+
+"""with open('my_style.qss', 'r') as s:
+    style = s.read()"""
 
 
 class DialogWindow(QDialog, Ui_Dialog):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.setStyleSheet(style)
 
         self.setupUi(self)
 
@@ -18,7 +25,24 @@ class DialogWindow(QDialog, Ui_Dialog):
     def on_button_okay(self):
         self.hide()
         self.main_window.show()
-        QMessageBox.information(self, 'Success', 'You are logged in!')
+
+        message = QMessageBox()
+        """message.critical(self, 'Success',
+                                    'Congratulations!\nYou are logged in!\nPlease proceed on creating your account')"""
+
+        message.setWindowTitle('PyQt6 Tutorial')
+        message.setText('This is the main text!\n\nJust adding more words to see how how this window will respond.\n'
+                        'So the height and width of the window responds to the number of text characters that it has.\n'
+                        '\nI wonder what the maximum height of this window is.')
+        message.setIcon(message.Icon.Information)
+        message.setStandardButtons(message.StandardButton.Ok)
+        message.setDefaultButton(message.StandardButton.Ok)
+        message.setInformativeText('This is informative!')
+
+        message.setDetailedText('Here are the details of the message.')
+
+        message.setStyleSheet(style)
+        message.exec()
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -32,8 +56,12 @@ if __name__ == '__main__':
     import sys
 
     app = QApplication(sys.argv)
+    app.setStyle('Fusion')
     window = DialogWindow()
+    # apply_stylesheet(app, theme='dark_blue.xml')
+
     window.show()
+
     sys.exit(app.exec())
 
     
@@ -110,3 +138,37 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.welcome_label.setText(_translate("MainWindow", "Welcome!"))
+        
+# =========== SHEET.PY =========== #
+    
+style = '''
+QPushButton {
+  color: white;
+  background-color: #5e19ff;
+  border-radius: 4px;
+  padding: 8px 16px ;
+  height: 18px;
+  text-transform: uppercase;
+  font-weight: bold;
+} 
+
+QPushButton:checked,
+QPushButton:pressed {
+  color: #ffffff;
+  background-color: #e4c047;
+}
+
+QPushButton.success:checked,
+QPushButton.success:pressed {
+  color: #ffffff;
+
+}
+
+QPushButton:hover {
+  background-color: #e4c047;
+}
+
+QIcon {
+    color: #ffffff;
+}
+'''
